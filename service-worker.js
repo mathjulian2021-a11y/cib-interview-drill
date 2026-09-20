0ae9f67f3c1dbb82c0e8bf5026c5a3d101123d2f
mathjulian2021-a11y <mathjulian2021@gmail.com>
2026-09-19 20:16:51 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'cib-drill-v1';
+const CACHE = 'cib-drill-v2';
 const CORE = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/core.js','./data/cases.js','./assets/icon-192.png','./assets/icon-512.png','./assets/icon-180.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
